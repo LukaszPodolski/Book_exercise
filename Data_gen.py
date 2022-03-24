08a48fa88b12b9fc9fee483495e43653db609a45
LukaszPodolski <nowynumer@gmail.com>
2022-03-24 23:21:00 +0100

    cz.dalsza

--- Data_gen.py
+++ Data_gen.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
-squers = [10107,8,86,6,756,556,65]
+
+x_values = [10,8,8,6,7,5,6]
+y_values  = [1,4,5,8,9,4,6]
 fig, ax = plt.subplots()
-ax.plot(squers)
+ax.plot(x_values)
+ax.scatter(x_values,y_values, s=11)
 plt.show()
